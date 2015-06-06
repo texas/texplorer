4efcde7a24ec64479b5efa75ee052d81a9023d2b
malev <marcosvanetta@gmail.com>
2015-06-06 16:46:19 -0500

    Show map

--- src/app.js
+++ src/app.js
@@ -5,3 +5,8 @@ $ = require('jquery');
 $.getJSON('http://localhost:9200/?q=', function (data) {
   console.log(data);
 });
+
+
+$(document).ready(function(){
+  var map = L.map('map').setView([30.22922, -97.756140], 13);
+});
