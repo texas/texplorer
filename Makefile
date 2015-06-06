87dde37c406dfd2af6f7f17f963e0893d9d29cb1
crccheck <c@crccheck.com>
2015-06-06 13:04:15 -0500

    get stuff in there

--- Makefile
+++ Makefile
@@ -1,10 +1,17 @@
 help:
 	@echo "help:"
 	@echo "-------------------------------------------"
-	@echo ""
-
+	@echo "es     Start an Elasticsearch Cluster"
+	@echo "index  Index the csv in Elasticsearch"
 
 es:
 	@docker rm -f ufo_es &>/dev/null || true
 	@sleep 1
 	docker run -d --name=ufo_es -p 9200:9200 elasticsearch
+
+index:
+	python main.py
+
+reindex:es
+	sleep 10
+	${MAKE} -s index
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from pprint import pprint
-
-import re
 import csv
+import os
+import re
+
+from elasticsearch import Elasticsearch
+
+
+INDEX = 'thc'
+DOC_TYPE = 'marker'
 
 
 def find_years(text):
@@ -21,15 +26,26 @@ def find_years(text):
 #             writer.writerow(row.tolist() + [cad])
 
 
-def read(path='data/Historical Marker_20150521_145030_254.csv'):
+def get_data(path='data/Historical Marker_20150521_145030_254.csv'):
     with open(path, 'r') as fp:
         reader = csv.DictReader(fp)
         for row in reader:
             text = row['markertext']
+            row['address'] = row['address'].strip()
             # add our own data
             row['years'] = find_years(text)
-            pprint(row)
-            break
+            yield row
 
 
-read()
+def push():
+    host = os.environ.get('ELASTICSEARCH_HOST', 'localhost')
+    connection = Elasticsearch([host])
+    for row in get_data():
+        connection.create(
+            index=INDEX,
+            doc_type=DOC_TYPE,
+            body=row,
+            id=row['atlas_number'],
+        )
+
+push()
