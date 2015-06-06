cdf498cac8baece2a0fb901d2ac364fd87eecd5d
malev <marcosvanetta@gmail.com>
2015-06-06 17:24:42 -0500

    And we have a map

--- src/app.js
+++ src/app.js
@@ -6,5 +6,15 @@ $.getJSON('http://localhost:9200/?q=', function (data) {
 
 
 $(document).ready(function(){
-  var map = L.map('map').setView([30.22922, -97.756140], 13);
+  var map = L.map('map', {
+    center: [30.2225, -97.7426],
+    zoom: 13,
+    zoomControl: false
+    });
+    window.map = map;  // DEBUG
+
+    L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.jpg', {
+     maxZoom: 18,
+     attribution: `map`,
+  }).addTo(map);
 });
