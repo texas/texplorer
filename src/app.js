f24f03c048553dfd9491e657e5763042bf51cded
crccheck <c@crccheck.com>
2015-06-08 23:25:57 -0500

    move map js to its own module

--- src/app.js
+++ src/app.js
@@ -1,55 +1,15 @@
 var _ = require('lodash');
+import * as mainMap from './map';
 var timeline = require('./timeline');
 var search = require('./search');
-var colors = require('./colors');
-var d3 = require('d3');
-if (process.env.NODE_ENV === 'development') {
-  window.d3 = d3;
-}
 
-var colorScale;
-var center = [30.2225, -97.7426];
-var southWest = L.latLng(26.603, -106.567);
-var northEast = L.latLng(36.927, -93.515);
-var bounds = L.latLngBounds(southWest, northEast);
-var map = L.map('map', {
-    center: center,
-    zoom: 12,
-    minZoom: 6,
-    maxBounds: bounds,
-    zoomControl: false
-  });
-  window.map = map;  // DEBUG
-
-  L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.jpg', {
-   maxZoom: 18,
-   attribution: 'map',
-}).addTo(map);
-var markersLayer = new L.LayerGroup();
-map.addLayer(markersLayer);
-map.locate({setView: true, maxZoom: 13});
-
-var markerLookup = {};
-var buildMarker = function(data, group) {
-  var html = `<h2>${data.title}</h2><p>${data.markertext}</p>`;
-
-  var marker =  L.circleMarker([data.location.lat, data.location.lon], {
-    color: d3.rgb(colors(data)).darker(1),
-    fillColor: colors(data),
-    fillOpacity: 0.8,
-    radius: 7
-  })
-    .bindPopup(html, {autoPan: false})
-    .addTo(markersLayer);
-  markerLookup[data.markernum] = marker;
-}
 
 function _gotResults(data) {
   if (process.env.NODE_ENV === 'development') {
     console.log(`got ${data.hits.hits.length} out of ${data.hits.total}`);
   }
-  markersLayer.clearLayers();
-  var bounds = map.getBounds();
+  mainMap.markersLayer.clearLayers();
+  var bounds = mainMap.map.getBounds();
   var visibleMarkers = _.filter(data.hits.hits,
         (x) => bounds.contains([x._source.location.lat, x._source.location.lon]))
   visibleMarkers = data.hits.hits
@@ -58,30 +18,29 @@ function _gotResults(data) {
     // TODO handle when there's nothing to show
     return;
   }
-  _.map(visibleMarkers, (element) => buildMarker(element._source, markersLayer));
+  _.map(visibleMarkers, (element) => mainMap.buildMarker(element._source, mainMap.markersLayer));
   timeline.init(visibleMarkers);
 }
 
 function doSearch() {
-  var bounds = map.getBounds();
+  var bounds = mainMap.map.getBounds();
   search(bounds.getNorthWest(), bounds.getSouthEast())
     .then(_gotResults);
 }
 
 var debouncedSearch = _.debounce(doSearch, 200)
 
-map.on('locationfound', function(loc) {
+mainMap.init();
+mainMap.map.on('locationfound', function(loc) {
   debouncedSearch();
 });
-
-map.on('moveend', function(result) {
+mainMap.map.on('moveend', function(result) {
   debouncedSearch();
 });
-
 // initial load
 doSearch();
 
 $('#timeline').on('ufoClick', function (e, a) {
-  var marker = markerLookup[a.markernum];
+  var marker = mainMap.markerLookup[a.markernum];
   marker && marker.openPopup();
 });
---
+++ src/map.js
@@ -0,0 +1,50 @@
+var d3 = require('d3');
+if (process.env.NODE_ENV === 'development') {
+  window.d3 = d3;
+}
+var colors = require('./colors');
+
+var center = [30.2225, -97.7426];
+var southWest = L.latLng(26.603, -106.567);
+var northEast = L.latLng(36.927, -93.515);
+var bounds = L.latLngBounds(southWest, northEast);
+
+export var map;
+export var markersLayer;
+export var markerLookup = {};
+
+export var buildMarker = function(data, group) {
+  var html = `<h2>${data.title}</h2><p>${data.markertext}</p>`;
+
+  var marker = L.circleMarker([data.location.lat, data.location.lon], {
+    color: d3.rgb(colors(data)).darker(1),
+    fillColor: colors(data),
+    fillOpacity: 0.8,
+    radius: 7
+  })
+    .bindPopup(html, {autoPan: false})
+    .addTo(markersLayer);
+  markerLookup[data.markernum] = marker;
+}
+
+export function init() {
+  map = L.map('map', {
+      center: center,
+      zoom: 12,
+      minZoom: 6,
+      maxBounds: bounds,
+      zoomControl: false
+    });
+    window.map = map;  // DEBUG
+
+    L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.jpg', {
+     maxZoom: 18,
+     attribution: 'map',
+  }).addTo(map);
+
+  markersLayer = new L.LayerGroup();
+  map.addLayer(markersLayer);
+
+  map.locate({setView: true, maxZoom: 13});
+
+}
